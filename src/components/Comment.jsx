907d395cd026d0ff3fdbb3b48043bbd2779c72ef
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-19 00:25:02 -0300

    feat: Comment box

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/BrunoCaputo.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Caputo</strong>
+              <time
+                title="18 de Setembro às 23:05h"
+                dateTime="18/09/2024 23:05:32"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
